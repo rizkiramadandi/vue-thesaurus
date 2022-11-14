e1d9b718ee6088692c14df2d825057650f636670
dandi16 <rramadandi16@gmail.com>
2022-11-14 19:55:21 +0700

    adding dist subtree

--- src/views/HomeView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>Welcome</h1>
-    <div>A simple Vue3 thesaurus project powered by <a href="https://www.datamuse.com/api/" rel="noreferrer noopenener" target="_blank">Datamuse API</a></div>
-  </div>
-</template>
